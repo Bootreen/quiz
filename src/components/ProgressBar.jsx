9e64cd192a81f5b77abce9375705b42a551e89a1
Oleksii Butrin <bootreen@gmail.com>
2024-07-01 14:34:23 +0200

    Added graphic progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,9 +1,25 @@
+import "./ProgressBar.css";
 import { quizQuestions } from "../data/questions";
+import { clsx } from "clsx";
 
 export const ProgressBar = ({ currQuestion }) => {
   return (
-    <div>
-      {currQuestion + 1} of {quizQuestions.length}
-    </div>
+    <>
+      <div className='progress-indicatior'>
+        {currQuestion + 1} of {quizQuestions.length}
+      </div>
+      <div className='progress-bar'>
+        {new Array(quizQuestions.length).fill(0).map((_, id) => (
+          <div
+            key={id}
+            className={clsx(
+              "progress-bit",
+              id <= currQuestion && "bit-active",
+              id === currQuestion && "bit-last"
+            )}
+          ></div>
+        ))}
+      </div>
+    </>
   );
 };
